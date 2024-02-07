1289df3b754817a25f0c3330448840eba2d82754
Sam Gardner <sam@wx4stg.com>
2024-02-07 14:47:04 -0600

    use tz-naive utcnow (older python versions)

--- services/check_fix_gps.py
+++ services/check_fix_gps.py
@@ -19,12 +19,12 @@ try:
         # Filter to mode 2 (2d fix) and mode 3 (3d fix)
         if sentence['class'] == 'TPV':
             if sentence['mode'] == 2  or sentence['mode'] == 3:
-                current_time = datetime.datetime.now(datetime.UTC).strftime('%Y-%m-%d %H:%M:%S.%f')
+                current_time = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')
                 print(f'[{current_time} GPS fixed, updating clock')
                 system('systemctl start update_clock_gps.service')
                 while True:
                     if system('systemctl status update_clock_gps') != 0 and system('systemctl status adc_test_startup') != 0:
-                        current_time = datetime.datetime.now(datetime.UTC).strftime('%Y-%m-%d %H:%M:%S.%f')
+                        current_time = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')
                         print(f'[{current_time} Clock updated and ADC test not running, starting data collect')
                         system('systemctl start adc_data_collect')
                         break
